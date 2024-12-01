3b1db6f30f63edc3d481177ee1822f3273e729fc
Tran Dang Duat <23020023@vnu.edu.vn>
2024-12-01 23:56:29 +0700

    gg

--- routes/ProjectRoutes.js
+++ routes/ProjectRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const ProjectController = require('../controllers/projectController');
+const ProjectController = require('../controllers/ProjectController');
 
 router.get('/projects', ProjectController.showProjectList);
 router.get('/projects/add', ProjectController.showAddProjectForm);
